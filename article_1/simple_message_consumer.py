c8aeb13ec205f9b64f99ff93ac8d703a95b92178
TrevorW-code <trwa4172@colorado.edu>
2022-06-04 13:13:21 -0600

    fixed some small errors

--- article_1/simple_message_consumer.py
+++ article_1/simple_message_consumer.py
@@ -4,14 +4,14 @@ import websockets
 
 logging.basicConfig(level=logging.INFO)
 
-async def consumer_handler(websocket: WebSocketclientProtocol) -> None:
+async def consumer_handler(websocket: WebSocketClientProtocol) -> None:
     async for message in websocket:
         log_message(message)
 
 async def consume(hostname: str, port: int) -> None:
     websocket_resource_url= f"ws://{hostname}:{port}"
-    async for message in websocket:
-        log_message(message)
+    async with websockets.connect(websocket_resource_url) as websocket:
+        await customer_handler(websocket)
 
 def log_message(message: str) -> None:
     logging.info(f"Message: {message}")
